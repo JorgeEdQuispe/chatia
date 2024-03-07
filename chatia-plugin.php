4719ed20af6aa8882b5ef8f5b414266efe2d28d7
Jorge Quispe <jorgequispe_456@hotmail.com>
2024-03-07 17:44:51 -0500

    creacion de chatia-functions php separado

---
+++ chatia-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Chatia
+Description: Plugin para mostrar un chatbot en WordPress.
+Version: 0.1
+Author: Jorge Quispe
+GitHub Plugin URI: https://github.com/tu_usuario/tu_repositorio
+Website: http://metricads.marketing
+Author URI: http://metricads.marketing
+*/
+
+
+// Incluir funciones
+require_once plugin_dir_path(__FILE__) . 'includes/chatia-functions.php';
