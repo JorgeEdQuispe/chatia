950634b3af89dfd477a56d060b45382ff42fdb88
Jorge Quispe <jorgequispe_456@hotmail.com>
2024-03-07 22:16:59 -0500

    para ver tailwind en php

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+
+module.exports = {
+    purge: {
+      content: [
+        './src/**/*.html',
+        './src/**/*.js',
+        './src/**/*.php', // Agregar archivos PHP
+      ],
+      // Otras opciones de configuración de purga...
+    },
+    // Otras opciones de configuración de Tailwind CSS...
+  }
